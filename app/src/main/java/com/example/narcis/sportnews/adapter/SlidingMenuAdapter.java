b878d050966106c1ed26d0e531a637848d08e039
narcismacinoi <narcis.macinoi@bcode.ro>
2016-02-10 13:17:11 +0200

    Changes.

---
+++ app/src/main/java/com/example/narcis/sportnews/adapter/SlidingMenuAdapter.java
@@ -0,0 +1,58 @@
+package com.example.narcis.sportnews.adapter;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.narcis.sportnews.R;
+import com.example.narcis.sportnews.model.ItemSlideMenu;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+/**
+ * Created by Narcis on 2/10/2016.
+ */
+public class SlidingMenuAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<ItemSlideMenu> lstItem;
+
+    public SlidingMenuAdapter(Context context, List<ItemSlideMenu> lstItem) {
+        this.context = context;
+        this.lstItem = lstItem;
+    }
+
+    @Override
+    public int getCount() {
+        return lstItem.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lstItem.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View v = View.inflate(context, R.layout.item_sliding_menu, null);
+        ImageView img = (ImageView) v.findViewById(R.id.item_img);
+        TextView tv = (TextView)v.findViewById(R.id.item_title);
+
+        ItemSlideMenu item = lstItem.get(position);
+        img.setImageResource(item.getImgId());
+        tv.setText(item.getTitle());
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/narcis/sportnews/fragment/Fragment1.java
@@ -0,0 +1,27 @@
+package com.example.narcis.sportnews.fragment;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.narcis.sportnews.R;
+
+
+public class Fragment1 extends Fragment {
+
+
+    public Fragment1() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment1, container, false);
+        return rootView;
+    }
+
+}
