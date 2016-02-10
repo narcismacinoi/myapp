53ea98d7d05531e8485fad06e17ee376c92b248b
narcismacinoi <narcis.macinoi@bcode.ro>
2016-02-11 00:48:24 +0200

    slide menu 1

--- app/src/main/java/com/example/narcis/sportnews/fragment/Fragment1.java
+++ app/src/main/java/com/example/narcis/sportnews/fragment/Fragment1.java
@@ -1,9 +1,6 @@
 package com.example.narcis.sportnews.fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,11 +8,10 @@ import android.view.ViewGroup;
 import com.example.narcis.sportnews.R;
 
 
-public class Fragment1 extends Fragment {
+public class Fragment1 extends android.app.Fragment {
 
 
     public Fragment1() {
-        // Required empty public constructor
     }
 
     @Override
